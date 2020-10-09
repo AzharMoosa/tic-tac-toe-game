00f39a7ea031ee63da44c344a06afaf523257f19
AzharMoosa <azhar.moosa8@gmail.com>
2020-10-09 19:38:18 +0100

    Player Menu

--- src/components/menu/MainMenu.js
+++ src/components/menu/MainMenu.js
@@ -19,17 +19,17 @@ const MainMenu = () => {
 
     return (
         <Fragment>
-        <div className={`main-menu ${!mainMenu && "hidden"}`}>
-            <h3>Welcome to Tic-Tac-Toe</h3>
-            <div className="btns">
-               <button className="btn btn-primary" onClick={toggleComputerMenu}>Play Against Computer</button>
-               <button className="btn btn-secondary" onClick={togglePlayerMenu}>Play Against Opponent</button>
+            <div className={`main-menu ${!mainMenu && "hidden"}`}>
+                <h3>Welcome to Tic-Tac-Toe</h3>
+                <div className="btns">
+                    <button className="btn btn-primary" onClick={toggleComputerMenu}>Play Against Computer</button>
+                    <button className="btn btn-secondary" onClick={togglePlayerMenu}>Play Against Opponent</button>
+                </div>
+                <h4>Created by <a target="_blank" rel="noopener noreferrer" className="highlight" href="https://github.com/AzharMoosa">Azhar Moosa</a></h4>
             </div>
-            <h4>Created by <a target="_blank" rel="noopener noreferrer" className="highlight" href="https://github.com/AzharMoosa">Azhar Moosa</a></h4>
-        </div>
 
-        <ComputerMenu computerMenu={computerMenu} setComputerMenu={setComputerMenu}/>
-        <PlayerMenu playerMenu={playerMenu} setPlayerMenu={setPlayerMenu} />
+            <ComputerMenu computerMenu={computerMenu} setComputerMenu={setComputerMenu}/>
+            <PlayerMenu playerMenu={playerMenu} setPlayerMenu={setPlayerMenu} />
         </Fragment>
     )
 }
--- src/components/menu/PlayerMenu.js
+++ src/components/menu/PlayerMenu.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const PlayerMenu = () => {
+const PlayerMenu = ({playerMenu, setPlayerMenu}) => {
+    const onClick = () => {
+        setPlayerMenu(false);
+    }
+    
     return (
-        <div>
-            
+        <div className={`player-menu main-menu ${!playerMenu && "hidden"}`}>
+            <h3>Play Against Opponent</h3>
+            <input type="text" className="player-input" placeholder="Enter Player One Name..." />
+            <input type="text" className="player-input" placeholder="Enter Player Two Name..." />
+            <button className="btn-small btn-secondary" onClick={onClick}>Play</button>
         </div>
     )
 }

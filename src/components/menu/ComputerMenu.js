9a11bd91775e4419f0389e731b04b149ab2fb907
AzharMoosa <azhar.moosa8@gmail.com>
2020-10-09 19:34:06 +0100

    Computer Menu

--- src/components/menu/ComputerMenu.js
+++ src/components/menu/ComputerMenu.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+ 
+const ComputerMenu = ({computerMenu, setComputerMenu}) => {
+    const onClick = () => {
+        setComputerMenu(false);
+    }
 
-const ComputerMenu = () => {
     return (
-        <div>
-            
+        <div className={`main-menu ${!computerMenu && "hidden"}`}>
+            <h3>Play Against Computer</h3>
+            <input type="text" className="player-input" placeholder="Enter Player Name..." />
+           <button className="btn-small btn-secondary" onClick={onClick}>Play</button>
         </div>
     )
 }
--- src/components/menu/MainMenu.js
+++ src/components/menu/MainMenu.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import ComputerMenu from "./ComputerMenu"
+import PlayerMenu from "./PlayerMenu"
 
 const MainMenu = () => {
+    const [mainMenu, setMainMenu] = useState(true);
+    const [computerMenu, setComputerMenu] = useState(false);
+    const [playerMenu, setPlayerMenu] = useState(false);
+
+    const togglePlayerMenu = () => {
+        setMainMenu(false);
+        setPlayerMenu(true);
+    }
+
+    const toggleComputerMenu = () => {
+        setMainMenu(false);
+        setComputerMenu(true);
+    }
+
     return (
-        <div className="main-menu">
+        <Fragment>
+        <div className={`main-menu ${!mainMenu && "hidden"}`}>
             <h3>Welcome to Tic-Tac-Toe</h3>
             <div className="btns">
-                <button className="btn btn-primary">Play Against Computer</button>
-                <button className="btn btn-secondary">Play Against Opponent</button>
+               <button className="btn btn-primary" onClick={toggleComputerMenu}>Play Against Computer</button>
+               <button className="btn btn-secondary" onClick={togglePlayerMenu}>Play Against Opponent</button>
             </div>
             <h4>Created by <a target="_blank" rel="noopener noreferrer" className="highlight" href="https://github.com/AzharMoosa">Azhar Moosa</a></h4>
         </div>
+
+        <ComputerMenu computerMenu={computerMenu} setComputerMenu={setComputerMenu}/>
+        <PlayerMenu playerMenu={playerMenu} setPlayerMenu={setPlayerMenu} />
+        </Fragment>
     )
 }
 

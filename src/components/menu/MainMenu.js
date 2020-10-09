5d16486fc18456cf2c01de2dd1f9d773e82a3417
AzharMoosa <azhar.moosa8@gmail.com>
2020-10-09 19:05:40 +0100

    Main Menu

--- src/components/menu/MainMenu.js
+++ src/components/menu/MainMenu.js
@@ -3,7 +3,12 @@ import React from 'react'
 const MainMenu = () => {
     return (
         <div className="main-menu">
-            
+            <h3>Welcome to Tic-Tac-Toe</h3>
+            <div className="btns">
+                <button className="btn btn-primary">Play Against Computer</button>
+                <button className="btn btn-secondary">Play Against Opponent</button>
+            </div>
+            <h4>Created by <a target="_blank" rel="noopener noreferrer" className="highlight" href="https://github.com/AzharMoosa">Azhar Moosa</a></h4>
         </div>
     )
 }
